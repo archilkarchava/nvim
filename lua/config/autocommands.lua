acb915f23b74291c80351a4695e4217330345c08
Archil Karchava <hello@archil.dev>
2023-06-07 19:49:36 +0400

    Move filetype autocommands into `config/autocommands.lua`

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -3,3 +3,21 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     vim.highlight.on_yank { higroup = "Visual", timeout = 250 }
   end,
 })
+
+if vim.g.vscode then
+  -- Fixes vim-matchup for scratch buffers in VS Code
+  vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+      if vim.bo.filetype == "" then
+        vim.bo.filetype = "on"
+      end
+    end,
+  })
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    callback = function()
+      if vim.bo.filetype == "" then
+        vim.cmd "filetype detect"
+      end
+    end
+  })
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -527,24 +527,6 @@ return {
       else
         vim.g.matchup_matchparen_deferred = 1
       end
-
-      if vim.g.vscode then
-        -- Fixes vim-matchup for scratch buffers in VS Code
-        vim.api.nvim_create_autocmd("BufEnter", {
-          callback = function()
-            if vim.bo.filetype == "" then
-              vim.bo.filetype = "on"
-            end
-          end,
-        })
-        vim.api.nvim_create_autocmd("BufWritePost", {
-          callback = function()
-            if vim.bo.filetype == "" then
-              vim.cmd "filetype detect"
-            end
-          end
-        })
-      end
     end,
   },
   {
