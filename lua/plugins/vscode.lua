3e49e3128f8735a19db6bc9cc48ca34bac530cd1
Archil Karchava <hello@archil.dev>
2023-07-06 21:07:22 +0400

    Easier way to enable/disable plugins in VS Code

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -9,31 +9,16 @@ local Config = require("lazy.core.config")
 local Plugin = require("lazy.core.plugin")
 Config.options.checker.enabled = false
 Config.options.change_detection.enabled = false
-
--- HACK: disable all plugins except the ones we want
-local fix_disabled = Plugin.Spec.fix_disabled
-function Plugin.Spec.fix_disabled(self)
-	for _, plugin in pairs(self.plugins) do
-		if not (vim.tbl_contains(enabled, plugin.name) or plugin.vscode) then
-			plugin.enabled = false
-		end
-	end
-	fix_disabled(self)
-end
-
--- HACK: don't clean plugins in vscode
-local update_state = Plugin.update_state
-function Plugin.update_state()
-	update_state()
-	Config.to_clean = {}
+Config.options.defaults.cond = function(plugin)
+	return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
 end
 
 return {
 	{
 		"archilkarchava/vscode.nvim",
 		vscode = true,
-    lazy = false,
-    priority = 1000,
+		lazy = false,
+		priority = 1000,
 		config = function()
 			require("vscode-theme").setup({})
 			require("vscode-theme").load()
