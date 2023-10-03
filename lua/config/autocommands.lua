19f3883f4ce96a4bc693a0d93eab50e4f4ea4eb0
Archil Karchava <hello@archil.dev>
2023-10-03 12:56:15 +0400

    Fix incremental search in VS Code

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -3,7 +3,9 @@ vim.api.nvim_set_hl(0, "YankColor", vim.g.vscode and { bg = "#264f78" } or { lin
 vim.on_key(function(char)
   if vim.fn.mode() == "n" then
     local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
-    if vim.opt.hlsearch:get() ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
+    if vim.opt.hlsearch:get() ~= new_hlsearch then
+      vim.cmd('nohlsearch')
+    end
   end
 end, vim.api.nvim_create_namespace("auto_hlsearch"))
 
